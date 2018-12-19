0dce5b9d7a7e0f86ca58427a8f5189d8334614f5
André <andrerrr@outlook.de>
2018-12-19 11:12:18 +0100

    Hallo Singelton (Not threadsafe!)

---
+++ HalloSingelton/HalloSingelton/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace HalloSingelton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Logger.Instance.Log("Hallo Log");
+
+            Logger.Instance.Log("Ende");
+            Console.ReadLine();
+        }
+    }
+
+    public class Logger
+    {
+        private static Logger logger = null;
+
+        public static Logger Instance
+        {
+            get
+            {
+                if (logger == null)
+                    logger = new Logger();
+                return logger;
+            }
+        }
+
+        private Logger()
+        { }
+
+        public void Log(string msg)
+        {
+            Console.WriteLine($"[{DateTime.Now}] {msg}");
+        }
+    }
+}
