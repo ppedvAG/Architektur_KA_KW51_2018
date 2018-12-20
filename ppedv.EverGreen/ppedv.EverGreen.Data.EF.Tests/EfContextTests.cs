2faf4e7ac114583493f123099d6891862aa1ce03
André <andrerrr@outlook.de>
2018-12-20 10:46:21 +0100

    Mit Tests noch besser

---
+++ ppedv.EverGreen/ppedv.EverGreen.Data.EF.Tests/EfContextTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ppedv.EverGreen.Model;
+
+namespace ppedv.EverGreen.Data.EF.Tests
+{
+    [TestClass]
+    public class EfContextTests
+    {
+        [TestMethod]
+        public void EfContext_can_create_database()
+        {
+            using (var con = new EfContext("Server=.;Database=EverGreen_CREATETEST;Trusted_Connection=true;"))
+            {
+                if (con.Database.Exists())
+                    con.Database.Delete();
+
+                con.Database.Create();
+                Assert.IsTrue(con.Database.Exists());
+            }
+        }
+
+        [TestMethod]
+        public void EfContext_can_add_Tannenbaum()
+        {
+            var baum = new Tannenbaum() { Height = 170, Width = 90, Price = 35.50m };
+
+            using (var con = new EfContext())
+            {
+                con.Tannenbaum.Add(baum);
+                con.SaveChanges();
+            }
+
+
+            using (var con = new EfContext())
+            {
+                var loaded = con.Tannenbaum.Find(baum.Id);
+
+                Assert.IsNotNull(loaded);
+                Assert.AreEqual(baum.Height, loaded.Height);
+            }
+        }
+    }
+}
---
+++ ppedv.EverGreen/ppedv.EverGreen.Data.EF.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ppedv.EverGreen.Data.EF.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ppedv.EverGreen.Data.EF.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e2ad3eae-e1e1-4439-aacf-0e26feaadde4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
