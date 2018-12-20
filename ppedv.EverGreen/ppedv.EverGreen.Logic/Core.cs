e830089c81a70a3c0650acb27dc2a621feeada65
André <andrerrr@outlook.de>
2018-12-20 14:28:41 +0100

    Demo Tannen

--- ppedv.EverGreen/ppedv.EverGreen.Logic/Core.cs
+++ ppedv.EverGreen/ppedv.EverGreen.Logic/Core.cs
@@ -1,11 +1,62 @@
-﻿using ppedv.EverGreen.Model.Contracts;
+﻿using ppedv.EverGreen.Model;
+using ppedv.EverGreen.Model.Contracts;
 using System;
+using System.Collections.Generic;
 
 namespace ppedv.EverGreen.Logic
 {
     public class Core
     {
         public IRepository Repository { get; private set; }
+
+
+        public IEnumerable<Tannenbaum> GetDemoData()
+        {
+            var ba1 = new BaumArt() { Name = "Weißtanne", Farbe = "Grün", Form = "Normal" };
+            var ba2 = new BaumArt() { Name = "Kilikische Tanne", Farbe = "Grün", Form = "Normal" };
+            var ba3 = new BaumArt() { Name = "Nordmanntanne", Farbe = "Grün", Form = "Normal" };
+
+            var h1 = new Herkunft() { Beschreibung = "Europa" };
+            var h2 = new Herkunft() { Beschreibung = "Kaukasus" };
+            var h3 = new Herkunft() { Beschreibung = "Anti-Taurus," };
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                yield return new Tannenbaum()
+                {
+                    BaumArt = ba1,
+                    Herkunft = h1,
+                    Height = 300 + (i * 33),
+                    Price = 3.3m * i,
+                    Width = 200 + (i * 22),
+                    Fällzeit = DateTime.Now.AddDays(i * 70 * -1)
+                };
+
+                yield return new Tannenbaum()
+                {
+                    BaumArt = ba2,
+                    Herkunft = h2,
+                    Height = 200 + (i * 33),
+                    Price = 4.7m * i,
+                    Width = 100 + (i * 22),
+                    Fällzeit = DateTime.Now.AddDays(i * 70 * -1)
+                };
+
+                yield return new Tannenbaum()
+                {
+                    BaumArt = ba2,
+                    Herkunft = h2,
+                    Height = 400 + (i * 33),
+                    Price = 2.4m * i,
+                    Width = 150 + (i * 22),
+                    Fällzeit = DateTime.Now.AddDays(i * 70 * -1)
+                };
+            }
+
+
+
+        }
 
         public Core(IRepository repo) //<----dependency injection in here
         {
