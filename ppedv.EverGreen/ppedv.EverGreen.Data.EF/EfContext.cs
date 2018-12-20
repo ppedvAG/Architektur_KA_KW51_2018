62fe8fa7592ddb6235f1b9e1f7af99f03ec7409d
André <andrerrr@outlook.de>
2018-12-20 10:46:12 +0100

    EF ist super

---
+++ ppedv.EverGreen/ppedv.EverGreen.Data.EF/EfContext.cs
@@ -0,0 +1,18 @@
+﻿using ppedv.EverGreen.Model;
+using System.Data.Entity;
+
+namespace ppedv.EverGreen.Data.EF
+{
+    public class EfContext : DbContext
+    {
+        public DbSet<BaumArt> BaumArt { get; set; }
+        public DbSet<Herkunft> Herkunft { get; set; }
+        public DbSet<Tannenbaum> Tannenbaum { get; set; }
+
+        public EfContext(string conString) : base(conString)
+        { }
+
+        public EfContext() : this("Server=.;Database=EverGreen_dev;Trusted_Connection=true;")
+        { }
+    }
+}
