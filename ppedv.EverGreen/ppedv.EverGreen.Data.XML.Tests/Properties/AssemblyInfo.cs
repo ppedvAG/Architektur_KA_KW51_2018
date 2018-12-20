99f56dc049ab5e4fb47ed7076d50d942c8765935
André <andrerrr@outlook.de>
2018-12-20 16:43:29 +0100

    Tests für das XML Repo

---
+++ ppedv.EverGreen/ppedv.EverGreen.Data.XML.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ppedv.EverGreen.Data.XML.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ppedv.EverGreen.Data.XML.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b9d882b8-b581-4379-a786-c6195ffb71c1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ppedv.EverGreen/ppedv.EverGreen.Data.XML.Tests/XmlRepoTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ppedv.EverGreen.Model;
+
+namespace ppedv.EverGreen.Data.XML.Tests
+{
+    [TestClass]
+    public class XmlRepoTests
+    {
+        [TestMethod]
+        public void XmlRepoTests_Can_CRUD_Baum()
+        {
+            var baum = new Tannenbaum() { Height = 17 };
+            var newHeight = 452;
+
+            {//INSERT
+                var repo = new XmlRepository();
+                repo.Add(baum);
+                repo.Save();
+            }
+
+
+            {//check Read
+                var repo = new XmlRepository();
+
+                Assert.IsTrue(repo.GetAll<Tannenbaum>().Any(x => x.Height == baum.Height));
+                var loaded = repo.GetById<Tannenbaum>(baum.Id);
+                Assert.IsNotNull(loaded);
+                Assert.AreEqual(baum.Height, loaded.Height);
+
+                //UPDATE
+                loaded.Height = 452;
+                repo.Save();
+            }
+
+            {//check UPDATE + DELETE
+                var repo = new XmlRepository(); 
+                var loaded = repo.GetById<Tannenbaum>(baum.Id);
+                Assert.AreEqual(newHeight, loaded.Height);
+
+                repo.Delete(loaded);
+                repo.Save();
+            }
+
+            {//check DELETE
+                var repo = new XmlRepository();
+                var loaded = repo.GetById<Tannenbaum>(baum.Id);
+                Assert.IsNull(loaded);
+            }
+        }
+
+        [TestMethod]
+        public void XmlRepo_add_sets_the_id()
+        {
+            var baum = new Tannenbaum() { Height = 17 };
+            Assert.AreEqual(0, baum.Id);
+
+            var repo = new XmlRepository();
+            repo.Add(baum);
+            Assert.AreNotEqual(0, baum.Id);
+        }
+    }
+}
