48da6260c544b3f6c3ca500bdbe8da27b7884c7e
André <andrerrr@outlook.de>
2018-12-20 12:23:32 +0100

    Core mit Repo in ConsoleGUI

--- ppedv.EverGreen/ppedv.EverGreen.Logic/Core.cs
+++ ppedv.EverGreen/ppedv.EverGreen.Logic/Core.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using ppedv.EverGreen.Model.Contracts;
+using System;
 
 namespace ppedv.EverGreen.Logic
 {
     public class Core
     {
-        public Core()
-        {
+        public IRepository Repository { get; private set; }
 
+        public Core(IRepository repo) //<----dependency injection in here
+        {
+            Repository = repo;
         }
+
+        public Core() : this(new Data.EF.EfRepository())
+        { }
     }
 }
---
+++ ppedv.EverGreen/ppedv.EverGreen.UI.DevConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using ppedv.EverGreen.Logic;
+using ppedv.EverGreen.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppedv.EverGreen.UI.DevConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("*** EverGreen v0.1 PREMIUM VERSION ***");
+
+            var core = new Core();
+
+            foreach (var b in core.Repository.GetAll<Tannenbaum>())
+            {
+                Console.WriteLine($"{b.BaumArt.Name} {b.Height}cm {b.Price:c}");
+            }
+
+            Console.WriteLine("Ende");
+            Console.ReadLine();
+        }
+    }
+}
